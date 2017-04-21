15ba30b7def4e3dd9d409e4191f449943c9de3de
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-04-21 14:40:50 -0400

    Moved the Steam API key and API endpoint URL to a separate file.

---
+++ steam_key.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Credientials for DGLcore.com for Steam API along with
+useful API endpoints
+*/
+
+
+//Steam API key
+$steamKey = 'FD7A4010D36B3ED66C98799206F51991';
+
+
+
+//Steam API endpoints
+
+//Steam User Info
+$IsteamURL ='http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002?key=';
+$suffix = '&steamids='
+
+?>
